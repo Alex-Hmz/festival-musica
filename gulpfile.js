0b1be55dcf8ae309bed67d67c68ebf95b6be51b8
Arturo HD <ale.hzdz.01@gmail.com>
2023-03-19 09:01:19 -0600

    Proyecto finalizado

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,26 @@ const { src, dest, watch, parallel } = require("gulp");
 //CSS
 const plumber = require ("gulp-plumber");
 const sass = require("gulp-sass")(require("sass"));
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const postcss = require("gulp-postcss");
+const sourcemaps = require("gulp-sourcemaps");
 
 //Imágenes
 const cache = require("gulp-cache");
 const imagemin = require("gulp-imagemin");
 const webp = require("gulp-webp");
 const avif = require("gulp-avif");
+//JS
+const terser = require("gulp-terser-js");
 
 function css (done){    
-    src("src/scss/**/*.scss") //Identificar archivo de SASS    
+    src("src/scss/**/*.scss") //Identificar archivo de SASS  
+        .pipe(sourcemaps.init())  
         .pipe(plumber())
         .pipe(sass()) //Compilar
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(sourcemaps.write("."))
         .pipe(dest("build/css")); //Guardar en HDD
 
     done();
@@ -54,7 +63,10 @@ function imagenes(done){
 //Función para llevarse archivos js al compilado
 function js(done){
     src("src/js/**/*.js")
-    .pipe(dest("build/js"));
+    .pipe(sourcemaps.init()) 
+    .pipe(terser())
+    .pipe(dest("build/js"))
+    .pipe(sourcemaps.write("."));
     done();
 }
 
